11a3340900bd6a1a758a54504c43054eb312cb70
Kirill Dyatlov <kirilldyatlovwebdev@gmail.com>
2022-11-11 04:24:49 +0200

    add mouse center position function

--- engine/Devices/Mouse.hpp
+++ engine/Devices/Mouse.hpp
@@ -25,6 +25,10 @@ namespace mtEngine
 
     void HideCursor() { glfwSetInputMode(Window::Get()->GetWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED); };
     void ShowCursor() { glfwSetInputMode(Window::Get()->GetWindow(), GLFW_CURSOR, GLFW_CURSOR_NORMAL); };
+    void SetCenterPosition() {
+      pos.x = (double) Window::Get()->GetSize()[0] / 2;
+      pos.y = (double) Window::Get()->GetSize()[1] / 2;
+    }
 
     Delegate<void(double, double)> onPositionEvent;
     Delegate<void(double, double)> onScrollEvent;
